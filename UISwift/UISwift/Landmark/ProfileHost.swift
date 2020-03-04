3620f5987d524309039bb565cea3e46b40d36948
zhangjingjian <zhangjingjian@gomo.com>
2020-03-04 18:42:27 +0800

    move files; add profile editing module;

---
+++ UISwift/UISwift/Landmark/ProfileHost.swift
@@ -0,0 +1,156 @@
+//
+//  ProfileHost.swift
+//  UISwift
+//
+//  Created by zjj on 2020/2/28.
+//  Copyright © 2020 zjj. All rights reserved.
+//
+
+import SwiftUI
+import BadgeUI
+import HikeUI
+
+struct HikeBadge: View {
+    var name: String
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibility(label: Text("Badge for \(name)."))
+        }
+    }
+}
+
+struct ProfileSummary: View {
+    var profile: Profile
+    static let goalFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    var body: some View {
+        List {
+            Text(profile.username)
+                .bold()
+                .font(.title)
+            
+            Text("Notifications: \(self.profile.prefersNotifications ? "On": "Off" )")
+            
+            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+            
+            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+            VStack(alignment: .leading) {
+                Text("Completed Badges")
+                    .font(.headline)
+                ScrollView {
+                    HStack {
+                        HikeBadge(name: "First Hike")
+
+                        HikeBadge(name: "Earth Day")
+                            .hueRotation(Angle(degrees: 90))
+
+                        HikeBadge(name: "Tenth Hike")
+                            .grayscale(0.5)
+                            .hueRotation(Angle(degrees: 45))
+                    }
+                }
+                .frame(height: 140)
+            }
+            VStack(alignment: .leading) {
+                Text("Recent Hikes")
+                    .font(.headline)
+                    .frame(height: 30)
+
+                HikeView(hike: hikeData[0])
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ProfileEditer: View {
+    @Binding var profile: Profile
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+            
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications").bold()
+            }
+            
+            VStack {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases, id: \.self) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            VStack {
+                Text("Goal Date").bold()
+                DatePicker("Goal Date", selection: $profile.goalDate, in: self.dateRange, displayedComponents: .date)
+            }
+        }
+    }
+}
+
+//struct ProfileEditer_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ProfileEditer(profile: .constant(.default))
+//    }
+//}
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var mode
+    @State var draftProfile = Profile.default
+    @EnvironmentObject var userData: UserData
+    var body: some View {
+        VStack {
+            HStack {
+                if self.mode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                    self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            if self.mode?.wrappedValue == .inactive {
+                ProfileSummary(profile: userData.profile)
+            } else {
+                ProfileEditer(profile: $draftProfile)
+                    .onAppear {
+                        self.draftProfile = self.userData.profile
+                    }
+                    .onDisappear {
+                        self.userData.profile = self.draftProfile
+                    }
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost().environmentObject(UserData())
+    }
+}
